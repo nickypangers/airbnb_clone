fa2fbb61eb4bbe0762658b2a3a1ac9b459ae99cb
nickypangers <nixonpang98@gmail.com>
2020-09-30 23:42:01 +0800

    initial commit

---
+++ src/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Header.css';
+
+function Header() {
+    return (
+        <div className='header'>
+            <img src="https://i.pinimg.com/originals/3c/bf/be/3cbfbe148597341fa56f2f87ade90956.png" alt="airbnb" className="header__icon"/>
+        </div>
+    )
+}
+
+export default Header
